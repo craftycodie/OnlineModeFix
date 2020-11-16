64c6f6b39278829239006737cce90ea65fe06318
Alex231 <git@alex231.com>
2020-11-16 18:07:31 +0000

    Fixed a bug where events were failing if the discord bridge was off.

--- src/gg/codie/mineonline/plugin/bukkit/MineOnlineBroadcastListener.java
+++ src/gg/codie/mineonline/plugin/bukkit/MineOnlineBroadcastListener.java
@@ -14,26 +14,31 @@ public class MineOnlineBroadcastListener extends PlayerListener {
 
     public void onPlayerQuit(PlayerQuitEvent event) {
         MineOnlineBroadcastPlugin.lastPing = 0;
-        discord.sendDiscordMessage("", "**" + colorCodeProvider.removeColorCodes(event.getPlayer().getName()) + "** left the game.");
+        if (discord != null)
+            discord.sendDiscordMessage("", "**" + colorCodeProvider.removeColorCodes(event.getPlayer().getName()) + "** left the game.");
     }
 
     public void onPlayerQuit(PlayerEvent event) {
         MineOnlineBroadcastPlugin.lastPing = 0;
-        discord.sendDiscordMessage("", "**" + colorCodeProvider.removeColorCodes(event.getPlayer().getName()) + "** left the game.");
+        if (discord != null)
+            discord.sendDiscordMessage("", "**" + colorCodeProvider.removeColorCodes(event.getPlayer().getName()) + "** left the game.");
     }
 
     public void onPlayerLogin(PlayerLoginEvent event) {
         MineOnlineBroadcastPlugin.lastPing = 0;
-        discord.sendDiscordMessage("", "**" + colorCodeProvider.removeColorCodes(event.getPlayer().getName()) + "** joined the game.");
+        if (discord != null)
+            discord.sendDiscordMessage("", "**" + colorCodeProvider.removeColorCodes(event.getPlayer().getName()) + "** joined the game.");
     }
 
     public void onPlayerKick(PlayerKickEvent event) {
         MineOnlineBroadcastPlugin.lastPing = 0;
-        discord.sendDiscordMessage("", "**" + colorCodeProvider.removeColorCodes(event.getPlayer().getName()) + "** left the game.");
+        if (discord != null)
+            discord.sendDiscordMessage("", "**" + colorCodeProvider.removeColorCodes(event.getPlayer().getName()) + "** left the game.");
     }
 
     @Override
     public void onPlayerChat(PlayerChatEvent event) {
-        discord.sendDiscordMessage(colorCodeProvider.removeColorCodes(event.getPlayer().getName()), colorCodeProvider.removeColorCodes(event.getMessage()));
+        if (discord != null)
+            discord.sendDiscordMessage(colorCodeProvider.removeColorCodes(event.getPlayer().getName()), colorCodeProvider.removeColorCodes(event.getMessage()));
     }
 }
