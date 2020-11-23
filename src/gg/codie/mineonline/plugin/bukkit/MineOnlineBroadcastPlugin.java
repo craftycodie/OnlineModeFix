4c49c87995308d3778fa7c72c3af5286dc11587e
RhysB <jetpackingwolf@gmail.com>
2020-11-23 21:19:54 +1000

    Change Bukkit plugin to use its own config

--- src/gg/codie/mineonline/plugin/bukkit/MineOnlineBroadcastPlugin.java
+++ src/gg/codie/mineonline/plugin/bukkit/MineOnlineBroadcastPlugin.java
@@ -35,6 +35,7 @@ public class MineOnlineBroadcastPlugin extends JavaPlugin {
     Logger log;
     ProxyThread proxyThread;
     DiscordChatBridge discord;
+    private MineOnlineConfig mineOnlineConfig;
 
     public void launchProxy() throws IOException {
         ServerSocket serverSocket = new ServerSocket(0);
@@ -56,7 +57,7 @@ public class MineOnlineBroadcastPlugin extends JavaPlugin {
     }
 
     public static byte[] createChecksum(String filename) throws Exception {
-        InputStream fis =  new FileInputStream(filename);
+        InputStream fis = new FileInputStream(filename);
 
         byte[] buffer = new byte[1024];
         MessageDigest complete = MessageDigest.getInstance("MD5");
@@ -89,7 +90,7 @@ public class MineOnlineBroadcastPlugin extends JavaPlugin {
         HttpURLConnection connection = null;
 
         try {
-            URLClassLoader classLoader = new URLClassLoader(new URL[] { MineOnlineBroadcastPlugin.class.getProtectionDomain().getCodeSource().getLocation() });
+            URLClassLoader classLoader = new URLClassLoader(new URL[]{MineOnlineBroadcastPlugin.class.getProtectionDomain().getCodeSource().getLocation()});
 
             Class jsonObjectClass = classLoader.loadClass("org.json.JSONObject");
 
@@ -108,12 +109,12 @@ public class MineOnlineBroadcastPlugin extends JavaPlugin {
             jsonObjectPut.invoke(jsonObject, "onlinemode", onlineMode);
             jsonObjectPut.invoke(jsonObject, "md5", md5);
             jsonObjectPut.invoke(jsonObject, "whitelisted", whitelisted);
-            if(!dontListPlayers)
+            if (!dontListPlayers)
                 jsonObjectPut.invoke(jsonObject, "players", playerNames);
             jsonObjectPut.invoke(jsonObject, "motd", motd);
             jsonObjectPut.invoke(jsonObject, "dontListPlayers", dontListPlayers);
 
-            String json = (String)jsonObjectToString.invoke(jsonObject);
+            String json = (String) jsonObjectToString.invoke(jsonObject);
 
             URL url = new URL("https://mineonline.codie.gg/api/servers");
             connection = (HttpURLConnection) url.openConnection();
@@ -152,49 +153,44 @@ public class MineOnlineBroadcastPlugin extends JavaPlugin {
 
         this.log.info("Enabled gg.codie.mineonline.plugin.bukkit.MineOnlineBroadcast");
 
+        mineOnlineConfig = new MineOnlineConfig(new File(this.getDataFolder(), "config.yml"));
         broadcastThread = new Thread(new Runnable() {
             @Override
             public void run() {
-                while(true) {
+                while (true) {
                     if (System.currentTimeMillis() - MineOnlineBroadcastPlugin.lastPing > 45000) {
                         lastPing = System.currentTimeMillis();
-                        try {
-                            Properties propertiesFile = new Properties();
-                            propertiesFile.load(new FileInputStream(new File("server.properties")));
 
-                            boolean isPublic = propertiesFile.getProperty("public", "true").equals("true");
-                            if(!isPublic)
-                                return;
+                        boolean isPublic = mineOnlineConfig.getConfigBoolean("public");
+                        if (!isPublic)
+                            return;
 
-                            String ip = propertiesFile.getProperty("serverlist-ip", propertiesFile.getProperty("server-ip", propertiesFile.getProperty("ip", null)));
-                            String port = propertiesFile.getProperty("serverlist-port", propertiesFile.getProperty("server-port", propertiesFile.getProperty("port", "25565")));
-                            int users = getServer().getOnlinePlayers().length;
-                            int maxUsers = Integer.parseInt(propertiesFile.getProperty("max-players", "20"));
-                            String name = propertiesFile.getProperty("server-name", "Minecraft Server");
-                            boolean onlineMode = propertiesFile.getProperty("online-mode", "true").equals("true");
-                            String md5 = propertiesFile.getProperty("version-md5", "");
-                            boolean whitelisted = propertiesFile.getProperty("whitelist", "false").equals("true");
-                            boolean dontListPlayers = propertiesFile.getProperty("dont-list-players", "false").equals("true");
-                            String motd = propertiesFile.getProperty("serverlist-motd", null);
+                        String ip = mineOnlineConfig.getConfigString("serverlist-ip");
+                        String port = mineOnlineConfig.getConfigString("serverlist-port");
+                        int users = getServer().getOnlinePlayers().length;
+                        int maxUsers = mineOnlineConfig.getConfigInteger("max-players");
+                        String name = mineOnlineConfig.getConfigString("server-name");
+                        boolean onlineMode = mineOnlineConfig.getConfigBoolean("online-mode");
+                        String md5 = mineOnlineConfig.getConfigString("version-md");
+                        boolean whitelisted = mineOnlineConfig.getConfigBoolean("whitelist");
+                        boolean dontListPlayers = mineOnlineConfig.getConfigBoolean("dont-list-players");
+                        String motd = mineOnlineConfig.getConfigString("serverlist-motd");
 
-                            String[] playerNames = Arrays.stream(getServer().getOnlinePlayers()).map(Player::getName).collect(Collectors.toList()).toArray(new String[users]);
+                        String[] playerNames = Arrays.stream(getServer().getOnlinePlayers()).map(Player::getName).collect(Collectors.toList()).toArray(new String[users]);
 
-                            listServer(
-                                    ip,
-                                    port,
-                                    users,
-                                    maxUsers,
-                                    name,
-                                    onlineMode,
-                                    md5,
-                                    whitelisted,
-                                    playerNames,
-                                    motd,
-                                    dontListPlayers
-                            );
-                        } catch (IOException ex) {
-                            // ignore.
-                        }
+                        listServer(
+                                ip,
+                                port,
+                                users,
+                                maxUsers,
+                                name,
+                                onlineMode,
+                                md5,
+                                whitelisted,
+                                playerNames,
+                                motd,
+                                dontListPlayers
+                        );
                     }
                 }
             }
@@ -254,7 +250,7 @@ public class MineOnlineBroadcastPlugin extends JavaPlugin {
                         String saneName = event.getAuthor().getName();
                         String saneMessage = sb.toString();
 
-                        if(saneMessage.trim().isEmpty())
+                        if (saneMessage.trim().isEmpty())
                             return;
 
                         Pattern trailingWhite = Pattern.compile(colorCodeProvider.getColorCode(EColorCodeColor.White) + "\\s{0,}$");
---
+++ src/gg/codie/mineonline/plugin/bukkit/MineOnlineConfig.java
@@ -0,0 +1,75 @@
+package gg.codie.mineonline.plugin.bukkit;
+
+import org.bukkit.util.config.Configuration;
+
+import java.io.File;
+
+public class MineOnlineConfig extends Configuration {
+
+
+    public MineOnlineConfig(File file) {
+        super(file);
+        this.reload();
+    }
+
+    private void write() {
+        //Main
+        generateConfigOption("config-version", 1);
+        //Setting
+        generateConfigOption("public", true);
+        generateConfigOption("serverlist-ip", null);
+        generateConfigOption("serverlist-port", 25565);
+        generateConfigOption("max-players", 20);
+        generateConfigOption("server-name", "Minecraft Server");
+        generateConfigOption("online-mode", true);
+        generateConfigOption("version-md", "");
+        generateConfigOption("whitelist", false);
+        generateConfigOption("dont-list-players", false);
+        generateConfigOption("serverlist-motd", null);
+
+    }
+
+    private void generateConfigOption(String key, Object defaultValue) {
+        if (this.getProperty(key) == null) {
+            this.setProperty(key, defaultValue);
+        }
+        final Object value = this.getProperty(key);
+        this.removeProperty(key);
+        this.setProperty(key, value);
+    }
+
+    //Getters Start
+    public Object getConfigOption(String key) {
+        return this.getProperty(key);
+    }
+
+    public String getConfigString(String key) {
+        return String.valueOf(getConfigOption(key));
+    }
+
+    public Integer getConfigInteger(String key) {
+        return Integer.valueOf(getConfigString(key));
+    }
+
+    public Long getConfigLong(String key) {
+        return Long.valueOf(getConfigString(key));
+    }
+
+    public Double getConfigDouble(String key) {
+        return Double.valueOf(getConfigString(key));
+    }
+
+    public Boolean getConfigBoolean(String key) {
+        return Boolean.valueOf(getConfigString(key));
+    }
+
+
+    //Getters End
+
+
+    private void reload() {
+        this.load();
+        this.write();
+        this.save();
+    }
+}
