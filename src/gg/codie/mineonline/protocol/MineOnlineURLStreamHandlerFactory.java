9722b99b7ae97bc3ac07f27bc3683b03003d13e7
craftycodie <craftycodie@outlook.com>
2022-12-21 17:04:01 +0000

    Appveyor'd

--- src/gg/codie/mineonline/protocol/MineOnlineURLStreamHandlerFactory.java
+++ src/gg/codie/mineonline/protocol/MineOnlineURLStreamHandlerFactory.java
@@ -2,14 +2,30 @@ package gg.codie.mineonline.protocol;
 
 import gg.codie.mineonline.protocol.http.Handler;
 
+import java.net.URL;
+import java.net.URLConnection;
 import java.net.URLStreamHandler;
 import java.net.URLStreamHandlerFactory;
 
 public class MineOnlineURLStreamHandlerFactory implements URLStreamHandlerFactory {
+    private final Class<? extends URLConnection> defaultHttpConnectionClass;
+
+    public MineOnlineURLStreamHandlerFactory() {
+        try {
+            URL foo = new URL("http://example.com");
+            // Doesn't actually establish a connection
+            defaultHttpConnectionClass = foo.openConnection().getClass();
+        } catch (Exception e) {
+            // this should never happen as the URL is hardcoded, shouldn't be invalid.
+            throw new RuntimeException(e);
+        }
+    }
+
+
     @Override
     public URLStreamHandler createURLStreamHandler(String protocol) {
         if ("http".equals(protocol)) {
-            return new Handler();
+            return new Handler(defaultHttpConnectionClass);
         }
 
         return null;
--- src/gg/codie/mineonline/protocol/http/Handler.java
+++ src/gg/codie/mineonline/protocol/http/Handler.java
@@ -4,17 +4,41 @@ import gg.codie.mineonline.protocol.CheckServerURLConnection;
 import sun.net.www.protocol.http.HttpURLConnection;
 
 import java.io.IOException;
+import java.net.Proxy;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLStreamHandler;
 
 public class Handler extends URLStreamHandler {
+    private final Class<? extends URLConnection> defaultHttpConnectionClass;
+
+    public Handler() {
+        // Necessary for vanilla servers which don't use the factory.
+        defaultHttpConnectionClass = null;
+    }
+
+    public Handler(Class<? extends URLConnection> _defaultHttpConnectionClass) {
+        defaultHttpConnectionClass = _defaultHttpConnectionClass;
+    }
     @Override
     protected URLConnection openConnection(URL url) throws IOException {
         // Online-Mode fix
         if (url.toString().contains("/game/checkserver.jsp"))
             return new CheckServerURLConnection(url);
-        else
-            return new HttpURLConnection(url, null);
+        else if (defaultHttpConnectionClass != null) {
+            try {
+                return defaultHttpConnectionClass.getConstructor(URL.class, Proxy.class).newInstance(url, null);
+            } catch (Exception e) {
+                // If the constructor isn't found, you can log that out. It's not expected.
+                return null;
+            }
+        } else {
+            try {
+                Class sunHttpConnection = ClassLoader.getSystemClassLoader().loadClass("sun.net.www.protocol.http.HttpURLConnection");
+                return (URLConnection) sunHttpConnection.getConstructor(URL.class, Proxy.class).newInstance(url, null);
+            } catch (Exception e) {
+                return null;
+            }
+        }
     }
 }
